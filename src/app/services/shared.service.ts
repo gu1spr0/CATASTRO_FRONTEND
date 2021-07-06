2719da662783690f725e0cec5fb3c4996a5ddfe6
Guillermo <learsi.guil2020@gmail.com>
2021-07-05 22:42:08 -0400

    Ultima modificaciones

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastMessage } from '../models/generic/ToastMessage';
+import { Observable } from 'rxjs';
+import { VarApis } from '../settings/VarApis';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _toastMessage: ToastMessage
+  ) { }
+
+  domainValues(domainCode: string): Observable<any>{
+    return this._http.get(`${VarApis.URL_DOMAINS}/value/select?domainCode=${domainCode}`);
+  }
+
+  showMessage(type: 'error' | 'success' | 'warning', message: string): void {
+    switch(type) {
+      case 'error':
+        this._toastMessage.error(message);
+        break;
+      case 'success':
+        this._toastMessage.success(message);
+        break;
+      case 'warning':
+        this._toastMessage.warning(message);
+        break;
+    }
+  }
 }
