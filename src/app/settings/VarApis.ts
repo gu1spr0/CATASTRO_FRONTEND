c010d1825d7d922b4e95d34e461ae3c57a7fbba4
iquispe <iquispe@datec.com.bo>
2021-08-28 19:32:25 -0400

    Avanze

--- src/app/settings/VarApis.ts
+++ src/app/settings/VarApis.ts
@@ -2,10 +2,11 @@ export class VarApis {
          static URL_API = 'http://localhost:9070/api';
          static URL_LOGIN = `${VarApis.URL_API}/login`;
          static URL_DOMAINS = `${VarApis.URL_API}/domains`;
+         static URL_DOMAINS_VALUE = `${VarApis.URL_API}/damainValue`
          static URL_USERS = `${VarApis.URL_API}/users`;
          static URL_RESOURCES = `${VarApis.URL_API}/resources`;
          static URL_ROUTES = `${VarApis.URL_API}/routes`;
          static URL_NOTARY = `${VarApis.URL_API}/notary`;
-         static ROLES = `${VarApis.URL_API}/roles`;
-         static REPOSITORY = `${VarApis.URL_API}/repo`;
+         static URL_ROLES = `${VarApis.URL_API}/roles`;
+         static URL_REPOSITORY = `${VarApis.URL_API}/repo`;
        }
